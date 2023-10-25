3c798cdeef0466274dd9fe6b7bef0a5584ce9cd5
Ben Clerix <34366781+benclerix-cegeka@users.noreply.github.com>
2023-10-25 09:35:01 +0200

    set default port

--- postSession.ps1
+++ postSession.ps1
@@ -1,4 +1,4 @@
-(Invoke-RestMethod -Method Post -Uri http://localhost:58898/sessions -Body (ConvertTo-Json @{
+(Invoke-RestMethod -Method Post -Uri http://localhost:5400/sessions -Body (ConvertTo-Json @{
 	ParkingFacilityId = "pf001"; 
 	CustomerId = "c001"; 
 	StartDateTime="2018-12-15 12:25:00";
