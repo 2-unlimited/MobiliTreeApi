cbdd148a2fc88aa46930b1969e6cc003f614775c
Ben Clerix <34366781+benclerix-cegeka@users.noreply.github.com>
2023-10-25 09:45:10 +0200

    correct post session body

--- postSession.ps1
+++ postSession.ps1
@@ -1,6 +1,6 @@
 (Invoke-RestMethod -Method Post -Uri http://localhost:5400/sessions -Body (ConvertTo-Json @{
 	ParkingFacilityId = "pf001"; 
 	CustomerId = "c001"; 
-	StartDateTime="2018-12-15 12:25:00";
-	EndDateTime="2018-12-15 14:05:00"
+	StartDateTime="2018-12-15T12:25:00";
+	EndDateTime="2018-12-15T14:05:00"
 }) -ContentType "application/json")
